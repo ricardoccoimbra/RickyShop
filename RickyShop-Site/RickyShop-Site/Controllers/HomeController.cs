9c57924003dbe6b18131064252b8daa6f4c7dedd
ricardoccoimbra <117987795+ricardoccoimbra@users.noreply.github.com>
2023-03-24 15:59:53 +0000

    1-Upload
    Criação do registo, login, forgot password

---
+++ RickyShop-Site/RickyShop-Site/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using RickyShop_Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RickyShop_Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        GestãoRickyShopEntities db = new GestãoRickyShopEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public ActionResult Registar()
+        {
+            return View();
+        }
+
+        [HandleError]
+        [HttpPost]
+        public ActionResult Registar(Utilizadores u)
+        {
+            try
+            {
+                var user = u;
+
+                var verif = Generic.VerifRegisto(u.NIF, u.Contacto, u.Email);
+
+                if (verif == "certo")
+                {
+                    u.Email = u.Email.ToLower();
+                    u.DataDeAdesao = DateTime.Now;
+                    u.DataDeNascimento = DateTime.Now;
+                    db.Utilizadores.Add(u);
+                   
+                    db.SaveChanges();
+                    Response.Write($"<script>alert('Conta Criada!')</script>");
+                    return RedirectToAction("Login");
+                }
+
+                if (verif == "nif")
+                {
+                    Response.Write($"<script>alert('NIF já existente!')</script>");
+
+                    return View();
+                }
+
+                if (verif == "email")
+                {
+                    Response.Write($"<script>alert('Email já existente!')</script>");
+                    u.Email = "";
+                    return View();
+                }
+
+                if (verif == "num")
+                {
+                    Response.Write($"<script>alert('Contacto já existente!')</script>");
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert({ex.Message});</script>");
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ RickyShop-Site/RickyShop-Site/Models/Generic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RickyShop_Site.Models
+{
+    public static class Generic
+    {
+        public static GestãoRickyShopEntities db = new GestãoRickyShopEntities();
+
+        public static string VerifRegisto(int _NIF, int _Contacto, string _Email)
+        {
+            string i = "certo";
+
+            var nif = db.Utilizadores.Count(s => s.NIF == _NIF);
+            var num = db.Utilizadores.Count(s => s.Contacto == _Contacto);
+            var mail = db.Utilizadores.Count(s => s.Email == _Email);
+
+            if (nif == 0)
+            {
+                if (num == 0)
+                {
+                    if (mail == 0)
+                    {
+                        return i;
+                    }
+                    else
+                    {
+                        i = "email";
+                        return i;
+                    }
+                }
+                else
+                {
+                    i = "num";
+                    return i;
+                }
+            }
+            else
+            {
+                i = "nif";
+                return i;
+            }
+        }
+
+    }
+}
